39971e205633b4206ec65a82149ee8186e296038
wrappor <wrapporr@gmail.com>
2022-08-04 18:56:26 -0400

    added fee storage and base data model

--- src/handlers/storeFees.ts
+++ src/handlers/storeFees.ts
@@ -8,9 +8,7 @@ import { handleAdapterError } from "../utils";
 import getAllChainsFromDexAdapters from "../utils/dexAdapters";
 import allSettled from 'promise.allsettled'
 import { importFeesAdapter } from "../utils/imports/importAdapter";
-
-// TODO: modify these 
-// import { storeVolume, Volume, VolumeType } from "../data/volume";
+import { storeFees, Fee, FeeType } from "../utils/data/fees";
 
 interface IHandlerEvent {
   protocolIndexes: number[]
@@ -27,20 +25,20 @@ export const handler = async (event: IHandlerEvent) => {
   // Timestamp to query, defaults current timestamp
   const currentTimestamp = event.timestamp || Date.now() / 1000;
   // Get clean day
-  const fetchCurrentHourTimestamp = getTimestampAtStartOfDayUTC(currentTimestamp);
+  const fetchCurrentDayTimestamp = getTimestampAtStartOfDayUTC(currentTimestamp);
 
   // Get closest block to clean day. Only for EVM compatible ones.
   const allChains = getAllChainsFromDexAdapters()
-  const chainBlocks = await getChainBlocks(fetchCurrentHourTimestamp, allChains);
+  const chainBlocks = await getChainBlocks(fetchCurrentDayTimestamp, allChains);
 
   async function runAdapter(feeAdapter: BaseAdapter, id: string, version?: string) {
     const chains = Object.keys(feeAdapter)
 
-    return allSettled(chains.map((chain) => feeAdapter[chain].fetch(fetchCurrentHourTimestamp, chainBlocks).then(result => ({ chain, result })).catch((e) => handleAdapterError(e, {
+    return allSettled(chains.map((chain) => feeAdapter[chain].fetch(fetchCurrentDayTimestamp, chainBlocks).then(result => ({ chain, result })).catch((e) => handleAdapterError(e, {
       id,
       chain,
       version,
-      timestamp: fetchCurrentHourTimestamp
+      timestamp: fetchCurrentDayTimestamp
     }))))
   }
 
@@ -48,47 +46,47 @@ export const handler = async (event: IHandlerEvent) => {
   const feeResponses = await Promise.all(event.protocolIndexes.map(async protocolIndex => {
     // Get info
     const { id, adapterKey } = feeAdapters[protocolIndex];
-    // Import adapter
-    const adapter: FeeAdapter = (await importFeesAdapter(feeAdapters[protocolIndex])).default;
 
-    // Retrieve daily volumes
-    let rawDailyFees: IRecordFeeData[] = []
-    if ("fees" in adapter) {
-      const runAdapterRes = await runAdapter(adapter.fees, id, "1")
-      console.log(runAdapterRes)
-      // TODO: process rejected promises
-      const fees = runAdapterRes.filter(rar => rar.status === 'fulfilled').map(r => r.status === "fulfilled" && r.value)
-      console.log(fees)
-      for (const fee of fees) {
-        console.log(fee)
-        if (fee)
-          console.log(fee.result)
-        if (fee && fee.result.dailyFees)
-          rawDailyFees.push({
-            [fee.chain]: {
-              [adapterKey]: +fee.result.dailyFees
-            },
-          })
+    try {
+      // Import adapter
+      const adapter: FeeAdapter = (await importFeesAdapter(feeAdapters[protocolIndex])).default;
+
+      // Retrieve daily volumes
+      let rawDailyFees: IRecordFeeData[] = []
+      if ("fees" in adapter) {
+        const runAdapterRes = await runAdapter(adapter.fees, id)
+        // TODO: process rejected promises
+        const fees = runAdapterRes.filter(rar => rar.status === 'fulfilled').map(r => r.status === "fulfilled" && r.value)
+        for (const fee of fees) {
+          if (fee && fee.result.dailyFees)
+            rawDailyFees.push({
+              [fee.chain]: {
+                [adapterKey]: +fee.result.dailyFees
+              },
+            })
+        }
+      } else {
+        console.error("Invalid adapter")
+        throw new Error("Invalid adapter")
       }
-    } else {
-      console.error("Invalid adapter")
-      throw new Error("Invalid adapter")
+      console.log("raw daily fees " + rawDailyFees)
+      const dailyFees = rawDailyFees.reduce((acc, current: IRecordFeeData) => {
+        const chain = Object.keys(current)[0]
+        acc[chain] = {
+          ...acc[chain],
+          ...current[chain]
+        }
+        return acc
+      }, {} as IRecordFeeData)
+      console.log("Retrieved", "fees", id, fetchCurrentDayTimestamp, dailyFees)
+
+      await storeFees(new Fee(FeeType.dailyFees, id, fetchCurrentDayTimestamp, dailyFees))
     }
-    console.log("raw daily fees " + rawDailyFees)
-    const dailyFees = rawDailyFees.reduce((acc, current: IRecordFeeData) => {
-      const chain = Object.keys(current)[0]
-      acc[chain] = {
-        ...acc[chain],
-        ...current[chain]
-      }
-      return acc
-    }, {} as IRecordFeeData)
-    console.log("Retrieved", "fees", id, fetchCurrentHourTimestamp, dailyFees)
-
-    // const v = new Volume(VolumeType.dailyVolume, id, fetchCurrentHourTimestamp, dailyVolumes)
-    // console.log("Retrieved", v, v.keys())
-    // await storeVolume(v)
-    // console.log("Stored", v.keys())
+    catch (error) {
+      const err = error as Error
+      console.error(error)
+      throw error
+    }
   }))
 
   // TODO: check if all adapters were success
---
+++ src/utils/data/fees.ts
@@ -0,0 +1,99 @@
+import { DynamoDB } from "aws-sdk"
+import dynamodb from "../dynamodb"
+import type { IRecordFeeData } from "../../handlers/storeFees"
+import { Item } from "./base"
+
+export enum FeeType {
+    dailyFees = "df"
+}
+
+export class Fee extends Item {
+    data: IRecordFeeData
+    type: FeeType
+    dexId: string
+    timestamp: number
+
+    constructor(type: FeeType, dexId: string, timestamp: number, data: IRecordFeeData) {
+        super()
+        this.data = data
+        this.type = type
+        this.dexId = dexId
+        this.timestamp = timestamp
+    }
+
+    static fromItem(item?: DynamoDB.AttributeMap): Fee {
+        if (!item) throw new Error("No item!")
+        if (!item.PK || !item.SK) throw new Error("Bad item!")
+        // PK=df#dex#{id}
+        // TODO: update dynamodb types with correct sdk
+        const dexId = (item.PK as string).split("#")[2]
+        const recordType = (item.PK as string).split("#")[0] as FeeType
+        const body = item as IRecordFeeData
+        const timestamp = +item.SK
+        delete body.PK;
+        delete body.SK;
+        return new Fee(recordType, dexId, timestamp, body)
+    }
+
+    get pk(): string {
+        return `${this.type}#dex#${this.dexId}`
+    }
+
+    get sk(): number {
+        return this.timestamp
+    }
+
+    toItem(): Record<string, unknown> {
+        return {
+            ...this.keys(),
+            ...this.data
+        }
+    }
+}
+
+export const storeFees = async (fee: Fee): Promise<Fee> => {
+    if (Object.entries(fee.data).length === 0) throw new Error("Can't store empty fee")
+    try {
+        await dynamodb.update({
+            Key: fee.keys(),
+            UpdateExpression: createUpdateExpressionFromObj(fee.data),
+            ExpressionAttributeValues: createExpressionAttributeValuesFromObj(fee.data)
+        }) // Upsert like
+        return fee
+    } catch (error) {
+        throw error
+    }
+}
+
+function createUpdateExpressionFromObj(obj: IRecordFeeData): string {
+    return `set ${Object.keys(obj).map(field => `${field}=:${field}`).join(',')}`
+}
+
+function createExpressionAttributeValuesFromObj(obj: IRecordFeeData): Record<string, unknown> {
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+        return {
+            ...acc,
+            [`:${key}`]: value
+        }
+    }, {} as Record<string, unknown>)
+}
+
+export const getFees = async (dex: string, type: FeeType, mode: "ALL" | "LAST" = "ALL"): Promise<Fee[] | Fee> => {
+    // Creating dummy object to get the correct key
+    const fee = new Fee(type, dex, null!, null!)
+    try {
+        const resp = await dynamodb.query({
+            // TODO: Change for upsert like
+            KeyConditionExpression: "PK = :pk",
+            ExpressionAttributeValues: {
+                ":pk": fee.pk,
+            },
+            Limit: mode === "LAST" ? 1 : undefined,
+            ScanIndexForward: mode === "LAST" ? false : undefined
+        })
+        if (!resp.Items || resp.Items.length === 0) throw Error(`No items found for ${fee.pk}`)
+        return mode === "LAST" ? Fee.fromItem(resp.Items[0]) : resp.Items.map(Fee.fromItem)
+    } catch (error) {
+        throw error
+    }
+}
