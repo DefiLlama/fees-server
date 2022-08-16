3dc4bfe828b9e51bcfd3de390795c28d506f51f1
wrappor <wrapporr@gmail.com>
2022-08-15 21:10:34 -0400

    added breakdown

--- src/utils/dexAdapters.ts
+++ src/utils/dexAdapters.ts
@@ -1,14 +1,14 @@
 import feeAdapters from "../adaptors";
 import { notUndefined } from "./adapterData"
 import { Chain } from "./constants";
-import { DexFeeAdapter } from "./adapters.type";
+import { FeeAdapter } from "./adapters.type";
 import data from "./protocols/data";
 
-const allFeeAdapters: Record<string, DexFeeAdapter> = feeAdapters;
+const allFeeAdapters: Record<string, FeeAdapter> = feeAdapters;
 
 const dexes = data.filter(d => d.category === "Dexes")
 
-const dexFeeAdapters: DexFeeAdapter[] = Object.keys(allFeeAdapters).map(adapterKey => {
+const dexFeeAdapters: FeeAdapter[] = Object.keys(allFeeAdapters).map(adapterKey => {
     const foundInProtocols = dexes.find(protocol =>
         protocol.name.toLowerCase()?.includes(adapterKey)
         || protocol.gecko_id?.includes(adapterKey)
@@ -25,6 +25,12 @@ const getAllChainsFromDexAdapters = Object.values(dexFeeAdapters).reduce((acc, d
         const chains = Object.keys(dexAdapter.fees) as Chain[]
         for (const chain of chains)
             if (!acc.includes(chain)) acc.push(chain)
+    } else if ("breakdown" in dexAdapter) {
+        for (const brokenDownDex of Object.values(dexAdapter.breakdown)) {
+            const chains = Object.keys(brokenDownDex) as Chain[]
+            for (const chain of chains)
+                if (!acc.includes(chain)) acc.push(chain)
+        }
     } else console.error("Invalid adapter")
     return acc
 }, [] as Chain[])
