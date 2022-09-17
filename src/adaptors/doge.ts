990a60426ec768586d60034526829dc8e45445ca
wrappor <wrapporr@gmail.com>
2022-09-17 04:10:04 -0400

    made helper function

--- src/adaptors/doge.ts
+++ src/adaptors/doge.ts
@@ -1,35 +1,11 @@
 import { FeeAdapter } from "../utils/adapters.type";
 import { DOGE } from "../helpers/chains";
-import { getTimestampAtStartOfPreviousDayUTC, getTimestampAtStartOfDayUTC } from "../utils/date";
-import { getOneDayFees } from "../helpers/getChainFees";
+import { chainAdapter } from "../helpers/getChainFees";
 
-
-const graphs = () => {
-  return () => {
-    return async (timestamp: number) => {
-      const today = new Date(getTimestampAtStartOfDayUTC(timestamp) * 1000).toISOString()
-      const yesterday = new Date(getTimestampAtStartOfPreviousDayUTC(timestamp) * 1000).toISOString()
-
-      const dailyFee = await getOneDayFees('doge', yesterday, today);
-  
-      return {
-        timestamp,
-        totalFees: "0",
-        dailyFees: dailyFee.toString(),
-        totalRevenue: "0",
-        dailyRevenue: "0",
-      };
-    };
-  };
-};
+const feeAdapter = chainAdapter(DOGE, "doge", 1386478800);
 
 const adapter: FeeAdapter = {
-  fees: {
-    [DOGE]: {
-        fetch: graphs()(),
-        start: 1386478800,
-    },
-  },
+  fees: feeAdapter,
   adapterType: "chain"
 }
 
