14acdd967a4b9c7c547a35f6583054418a7c8faa
dtmkeng <phumaisut@gmail.com>
2022-09-19 21:51:24 +0700

    add wombat

--- src/utils/imports/adaptors.ts
+++ src/utils/imports/adaptors.ts
@@ -25,4 +25,5 @@ export default {
     "litecoin": require("../../adaptors/litecoin"),
     "doge": require("../../adaptors/doge"),
     "biswap": require("../../adaptors/biswap"),
+    "wombat-exchange": require("../../adaptors/wombat-exchange"),
 }
