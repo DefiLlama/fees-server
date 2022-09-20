7ca30faad311823068a345c31e60c997fc8ee6e7
dtmkeng <phumaisut@gmail.com>
2022-09-20 08:46:40 +0700

    fix total fee include Stable swap

--- src/adaptors/spiritswap.ts
+++ src/adaptors/spiritswap.ts
@@ -2,7 +2,7 @@ import { FeeAdapter } from "../utils/adapters.type";
 import volumeAdapter from "@defillama/adapters/volumes/adapters/spiritswap";
 import { getDexChainFees } from "../helpers/getUniSubgraphFees";
 
-const TOTAL_FEES = 0.0018;
+const TOTAL_FEES = 0.0022;
 const PROTOCOL_FEES = 0.00045;
 
 const feeAdapter = getDexChainFees({
